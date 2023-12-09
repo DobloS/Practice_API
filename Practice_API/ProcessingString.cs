17b1e3c069474726f57a4e2ec49d54b712a0d0c3
DobloS <s.maks0208@mail.ru>
2023-12-09 18:23:50 +0500

    Решение

--- Practice_API/ProcessingString.cs
+++ Practice_API/ProcessingString.cs
@@ -2,12 +2,6 @@ namespace Practice_API
 {
     public class ProcessingString
     {
-        public DateTime Date { get; set; }
 
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
     }
 }
